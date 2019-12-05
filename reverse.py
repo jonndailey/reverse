d7f673f5122bbca1faa396e53dda5bff10e589dd
Aeriel Denmark <aeriel.denmark@gmail.com>
2019-12-04 19:26:50 -0500

    Reverse

---
+++ reverse.py
@@ -0,0 +1,12 @@
+# Reverse
+# Aeriel Denmark
+# Wed 12/4/19
+
+# Write a program that prints a given string or array
+# in reverse
+
+my_string = ["a", "b", "c"]
+
+my_string.reverse()
+
+print(my_string)
